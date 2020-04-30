81b86b5413e2840ed83455319fb5b89d451a826e
yaoqing <yaoqing-hb@bestpay.com.cn>
2020-04-30 17:12:11 +0800

    优化-> 执行用时:2ms; 内存消耗:40.3MB

--- src/main/java/com/linked/list/AddTwoNumbersSolution.java
+++ src/main/java/com/linked/list/AddTwoNumbersSolution.java
@@ -13,7 +13,6 @@ public class AddTwoNumbersSolution {
         ListNode result = new ListNode(0);
         ListNode reNext = result;
         boolean isCarry = false;
-        ListNode zeroNode = new ListNode(0);
 
         // 全部个位数加
         if(l1.next == null && l2.next == null) {
@@ -26,24 +25,23 @@ public class AddTwoNumbersSolution {
             return result;
         }
 
-        while (l1 != null || l2 != null || isCarry) {
-            if (l1 == null) {
-                l1 = zeroNode;
-            }
-
-            if (l2 == null) {
-                l2 = zeroNode;
-            }
-            int val = l1.val + l2.val + (isCarry ? 1 : 0);
+        while (true) {
+            int x = (l1 == null) ? 0 : l1.val;
+            int y = (l2 == null) ? 0 : l2.val;
+            int sum = x + y + (isCarry ? 1 : 0);
             isCarry = false;
-            if(val > 9) {
+            if(sum > 9) {
                 isCarry = true;
-                val = val - 10;
+                sum = sum - 10;
             }
-            reNext.val = val;
+            reNext.val = sum;
 
-            l1 = l1.next;
-            l2 = l2.next;
+            if(l1 != null ) {
+                l1 = l1.next;
+            }
+            if(l2 != null) {
+                l2 = l2.next;
+            }
 
             if(l1 == null && l2 == null && !isCarry) {
                 break;
@@ -55,11 +53,13 @@ public class AddTwoNumbersSolution {
         return result;
     }
 
+
+
     public static void main(String[] args) {
-        ListNode l1 = new ListNode(6);
-        l1.next = new ListNode(4);
-        ListNode l1next1 = l1.next;
-        l1next1.next = new ListNode(5);
+        ListNode l1 = new ListNode(1);
+//        l1.next = new ListNode(4);
+//        ListNode l1next1 = l1.next;
+//        l1next1.next = new ListNode(5);
 //        ListNode l1next2 = l1next1.next;
 //        l1next2.next = new ListNode(9);
 //        l1next2.next.next = new ListNode(7);
@@ -67,11 +67,11 @@ public class AddTwoNumbersSolution {
 
 
         ListNode l2 = new ListNode(9);
-        l2.next = new ListNode(8);
-        l2.next.next = new ListNode(7);
-//        ListNode l2next = l2;
-//        l2next.val = 9;
-//        System.out.println(l2.val);
+//        l2.next = new ListNode(9);
+//        l2.next.next = new ListNode(7);
+//        l2.next.next.next = new ListNode(7);
+//        l2.next.next.next.next = new ListNode(7);
+//        l2.next.next.next.next.next = new ListNode(7);
         ListNode sum = addTwoNumbers(l1, l2);
 
         System.out.println(sum.val);
